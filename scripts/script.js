7038998ce072ebd4f1c2852ff83e72e354b2feec
Ander Swift <anderswift@gmail.com>
2020-08-22 22:31:17 -0400

    feat: function to save edited profile info

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,38 @@
-let container= document.querySelector('.container');
+let profile= document.querySelector('.profile');
+let editInfoButton= profile.querySelector(".profile__edit-info");
+let profileName= profile.querySelector(".profile__name");
+let profileAbout= profile.querySelector(".profile__about");
 
 let modal= document.querySelector(".modal");
-let editInfoButton= container.querySelector(".profile__edit-info");
+let modalForm= modal.querySelector(".modal__form");
+let modalName= modal.querySelector(".modal__input_name");
+let modalAbout= modal.querySelector(".modal__input_about");
 let exitButton= modal.querySelector(".modal__exit");
 
-editInfoButton.addEventListener("click", function () {
+
+function openProfileForm() {
+	modalName.value= profileName.textContent;
+	modalAbout.value= profileAbout.textContent;
+	
 	modal.classList.remove('modal_hidden');
-});
+}
 
-exitButton.addEventListener("click", function () {
-  modal.classList.add('modal_hidden');
-});
 
+function exitProfileForm() {
+	modal.classList.add('modal_hidden');
+}
+
+
+function formSubmitHandler(e) {
+	e.preventDefault(); 
+	
+	profileName.textContent= modalName.value;
+	profileAbout.textContent= modalAbout.value;
+	
+	exitProfileForm();
+}
+
+
+editInfoButton.addEventListener("click", openProfileForm);
+exitButton.addEventListener("click", exitProfileForm);
+modalForm.addEventListener('submit', formSubmitHandler);
